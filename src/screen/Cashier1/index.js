6240b4aa05045c6b8a3bd90c6698e1adbc47b332
JosuaLimbu <osaltusal@gmail.com>
2023-05-04 21:01:16 +0800

    add arrow pointing

--- src/screen/Cashier1/index.js
+++ src/screen/Cashier1/index.js
@@ -4,12 +4,12 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 const Cashier1 = ({ onBackPress }) => {
   return (
     <View style={styles.navbar}>
-      {/* <TouchableOpacity style={styles.backButton} onPress={onBackPress}>
+      <TouchableOpacity style={styles.backButton} onPress={onBackPress}>
         <Image
-          source={require('./path/to/your/image.png')}
+          source={require('../../assets/icon/ArrowPointingLeft.png')}
           style={styles.backButtonImage}
         />
-      </TouchableOpacity> */}
+      </TouchableOpacity>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Cashier</Text>
       </View>
@@ -19,12 +19,11 @@ const Cashier1 = ({ onBackPress }) => {
 }
 
 export default Cashier1;
+
 const styles = StyleSheet.create({
   navbar: {
     height: 64,
-    fontColor: '#303B3B',
     flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'space-between',
     paddingLeft: 30,
     paddingRight: 30,
@@ -39,25 +38,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   backButtonImage: {
+    position: 'absolute',
     width: '100%',
     height: '100%',
   },
   titleContainer: {
     flex: 1,
-    alignItems: 'center',
   },
   title: {
-    fontFamily: 'Poppins',
+    height: 27,
+    top: 30,
+    left: '50%',
+    marginLeft: -36,
     fontWeight: '600',
     fontSize: 18,
     lineHeight: 27,
     color: '#303B3B',
+    fontFamily: 'Poppins-SemiBold',
   },
   separator: {
     position: 'absolute',
     top: 64,
-    left: 45,
-    width: 315,
+    left: 30,
+    width: 345,
     height: 1,
     backgroundColor: '#ABABA7',
     opacity: 0.38,
