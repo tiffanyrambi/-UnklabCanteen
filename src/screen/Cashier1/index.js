d3cb39c572dffd486dbbb47342e801ee9b7f3fc0
JosuaLimbu <osaltusal@gmail.com>
2023-05-08 20:57:29 +0800

    tambah atoms

--- src/screen/Cashier1/index.js
+++ src/screen/Cashier1/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import BackButton from '../atoms/BackButton';
 
-const Cashier1 = ({ onBackPress }) => {
+const Cashier1 = () => {
   return (
     <View style={styles.navbar}>
-      <TouchableOpacity style={styles.backButton} onPress={onBackPress}>
-        <Image
-          source={require('../../assets/icon/ArrowPointingLeft.png')}
-          style={styles.backButtonImage}
-        />
-      </TouchableOpacity>
+      <BackButton/>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Cashier</Text>
       </View>
@@ -27,20 +23,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingLeft: 30,
     paddingRight: 30,
-  },
-  backButton: {
-    position: 'absolute',
-    width: 25,
-    height: 25,
-    left: 30,
-    top: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  backButtonImage: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
   },
   titleContainer: {
     flex: 1,
