e64fbe064d5a062abb36efc9aaf6e369ba2af09b
angeltrianypangkey <angeltriany@gmail.com>
2023-05-08 20:15:09 +0800

    splashscreen

---
+++ App.js
@@ -0,0 +1,26 @@
+//import
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SplashScreen} from './src/Screens';
+
+//Function Component
+const Stack = createNativeStackNavigator();
+const App = () => {
+    return (
+        <NavigationContainer>
+        <Stack.Navigator
+            initialRouteName='SplashScreen'>
+            <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}
+            options={{headerShown: false}}
+            /> 
+
+        </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+//Export
+export default App;
---
+++ src/Screens/SplashScreen/index.js
@@ -0,0 +1,48 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
+const SplashScreen = ({navigation}) => {
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.replace('SignIn');
+        }, 3000);
+    }, []);
+
+    return (
+        <LinearGradient
+            colors={['#541690', '#FFFFFF']} // Ganti warna sesuai keinginan
+            start={{x: 0.5, y: 0}}
+            end={{x: 0.5, y: 2}}
+            style={styles.container}>
+            <Text style={styles.text1}>Unklab</Text>
+            <Text style={styles.text2}>Canteen</Text>
+        </LinearGradient>
+    );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+      
+    text1: {
+        fontSize: 40,
+        fontFamily: 'MajorMonoDisplay-Regular',
+        color: 'white',
+        margin: 1,
+        paddingLeft: 50,
+        paddingRight: 110,
+    },
+    text2: {
+        fontSize: 40,
+        fontFamily: 'MajorMonoDisplay-Regular',
+        color: 'white',
+        paddingLeft: 110,
+        paddingRight: 50,
+    },
+});
